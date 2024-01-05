80048302c139f6716e3bd51658e420146df38a12
Keith Taylor <68087065+Swift928@users.noreply.github.com>
2024-01-05 23:07:17 +0100

    attacks and game status is now functional

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -50,80 +50,84 @@ class GameLoop {
 
     async playGame() {
         const computerField = document.getElementById('computerField');
-        const playerField = document.getElementById('playerField');
 
         while (!this.isGameOver) {
-            const playerPromise = new Promise((resolve) => {
-                computerField.addEventListener('click', (event) => {
-                    if (event.target.classList.contains('grid-item')) {
-                        const index = Array.from(
-                            event.target.parentNode.children
-                        )
-                            .filter(
-                                (item) => !item.classList.contains('overlay')
-                            )
-                            .indexOf(event.target);
-
-                        const rowIndex = Math.floor(index / 10);
-                        const colIndex = index % 10;
-
-                        this.player.opponentBoard.receiveAttack(
-                            rowIndex,
-                            colIndex
-                        );
-
-                        if (
-                            this.player.opponentBoard.field[rowIndex][
-                                colIndex
-                            ] === 'hit'
-                        ) {
-                            const dot = document.createElement('div');
-                            dot.classList.add('dot');
-                            event.target.appendChild(dot);
-                        }
-
-                        event.target.classList.add('used-item');
-                        this.boardCheck();
-                        resolve();
-                    }
-                });
-            });
-
-            await playerPromise;
+            await this.playerMove();
+            if (this.isGameOver) {
+                break;
+            }
 
             this.switchTurns();
             computerField.style.pointerEvents = 'none';
 
-            const computerPromise = new Promise(async (resolve) => {
-                const { xCor, yCor } = await this.computer.computerAttack();
-
-                const gridElements = Array.from(playerField.children).filter(
-                    (item) => !item.classList.contains('overlay')
-                );
-
-                const index = xCor * 10 + yCor;
-
-                const element = gridElements[index];
-
-                if (this.computer.opponentBoard.field[xCor][yCor] === 'hit') {
-                    const dot = document.createElement('div');
-                    dot.classList.add('dot');
-
-                    element.appendChild(dot);
-                }
-                element.classList.add('used-item');
-
-                this.boardCheck();
-                resolve();
-            });
-
-            await computerPromise;
+            await this.computerMove();
+            if (this.isGameOver) {
+                break;
+            }
 
             this.switchTurns();
             computerField.style.pointerEvents = 'auto';
         }
 
+        computerField.style.pointerEvents = 'none';
         return console.log(`${this.activePlayer} You Win!`);
+    }
+
+    async playerMove() {
+        const computerField = document.getElementById('computerField');
+
+        return new Promise((resolve) => {
+            computerField.addEventListener('click', (event) => {
+                if (event.target.classList.contains('grid-item')) {
+                    const index = Array.from(event.target.parentNode.children)
+                        .filter((item) => !item.classList.contains('overlay'))
+                        .indexOf(event.target);
+
+                    const rowIndex = Math.floor(index / 10);
+                    const colIndex = index % 10;
+
+                    this.player.opponentBoard.receiveAttack(rowIndex, colIndex);
+
+                    if (
+                        this.player.opponentBoard.field[rowIndex][colIndex] ===
+                        'hit'
+                    ) {
+                        const dot = document.createElement('div');
+                        dot.classList.add('dot');
+                        event.target.appendChild(dot);
+                    }
+
+                    event.target.classList.add('used-item');
+                    this.boardCheck();
+                    resolve();
+                }
+            });
+        });
+    }
+
+    async computerMove() {
+        const playerField = document.getElementById('playerField');
+        return new Promise(async (resolve) => {
+            const { xCor, yCor } = await this.computer.computerAttack();
+
+            const gridElements = Array.from(playerField.children).filter(
+                (item) => !item.classList.contains('overlay')
+            );
+
+            const index = xCor * 10 + yCor;
+
+            const element = gridElements[index];
+
+            if (this.computer.opponentBoard.field[xCor][yCor] === 'hit') {
+                const dot = document.createElement('div');
+                dot.classList.add('dot');
+
+                element.appendChild(dot);
+            }
+            element.classList.add('used-item');
+            this.boardCheck();
+            resolve();
+        });
     }
 
     switchTurns() {
@@ -134,11 +138,9 @@ class GameLoop {
     }
 
     boardCheck() {
-        const ships = new Set(
-            this.computer.playerField.field
-                .flat()
-                .filter((field) => field instanceof Ship)
-        );
+        const ships = this.computer.playerField.field
+            .flat()
+            .filter((field) => field instanceof Ship);
 
         if (Array.from(ships).every((ship) => ship.sunk === true)) {
             this.endGame();
@@ -153,6 +155,7 @@ class GameLoop {
         const computerField = document.getElementById('computerField');
 
         computerField.classList.add('game-over');
+        computerField.style.pointerEvents = 'none';
     }
 }
 
