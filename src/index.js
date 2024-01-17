005efcb760ec0be579dbafe4ff90c8839b4773eb
Keith Taylor <68087065+Swift928@users.noreply.github.com>
2024-01-17 16:41:42 +0100

    Refactored index.js, End game now functional

--- src/index.js
+++ src/index.js
@@ -12,46 +12,53 @@ const { GameLoop } = require('./gameFunctions/gameLoop');
     const computerFieldElement = document.getElementById('computerField');
     const gameFields = document.getElementById('gameFields');
     const endOfGameContainer = document.getElementById('endOfGameContainer');
+    const formsDiv = document.getElementById('nameInputDiv');
+    const playAgainButton = document.querySelector('#playAgainButton');
+    const endGameButton = document.querySelector('#endGameButton');
 
-    const startGame = async () => {
-        if (!game) game = new GameLoop();
-
-        if (!state) {
-            state = await screenRender();
-        } else {
-            state = await screenRender(state.playerName);
-        }
-
-        gameFields.classList.remove('hidden');
-        playerFieldElement.style.display = 'grid';
-        computerFieldElement.style.display = 'grid';
-
-        await game.setPlayer(state.playerName, state.playerShipPlacement);
+    const renderBoard = () => {
         const playerField = game.player.playerField.field;
         const computerField = game.computer.playerField.field;
 
         FieldCreation.renderBoard(playerFieldElement, playerField);
         FieldCreation.renderBoard(computerFieldElement, computerField);
+    };
+
+    const startGame = async () => {
+        if (!game) game = new GameLoop();
+
+        state = await screenRender(state?.playerName);
+
+        gameFields.classList.remove('hidden');
+        [playerFieldElement, computerFieldElement].forEach(
+            (el) => (el.style.display = 'grid')
+        );
+
+        await game.setPlayer(state.playerName, state.playerShipPlacement);
+
+        renderBoard();
 
         game.playGame();
     };
 
     startGame();
 
-    const playAgainButton = document.querySelector('#playAgainButton');
-    const endGameButton = document.querySelector('#endGameButton');
-
     playAgainButton.addEventListener('click', async () => {
-        const formsDiv = document.getElementById('nameInputDiv');
-
-        endOfGameContainer.classList.toggle('hidden');
-        formsDiv.classList.toggle('hidden');
+        [endOfGameContainer, formsDiv].forEach((el) =>
+            el.classList.toggle('hidden')
+        );
 
         game.resetGame();
 
-        playerFieldElement.innerHTML = '';
-        computerFieldElement.innerHTML = '';
+        [playerFieldElement, computerFieldElement].forEach(
+            (el) => (el.innerHTML = '')
+        );
 
         await startGame();
+    });
+
+    endGameButton.addEventListener('click', () => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
     });
 })();
